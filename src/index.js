6191575828782845660030404d51e8e8f695ddf3
Andrei Lukyanau <lukyanoff@tut.by>
2021-08-04 22:49:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+	n = String(n);
+	let index = n.length - 1;
+	let reversedN = '';
+	while (index >= 0) {
+		if (n[index] == '-') {
+            index = --index;
+         }
+		else {
+			const currentChar = n[index];
+			reversedN = `${reversedN}${currentChar}`;
+			index = --index;
+		}
+	}
+	return reversedN;
 }
